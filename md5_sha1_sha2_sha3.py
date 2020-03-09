ec97c5a19e1a826845ef1c57f06c86b9a53148db
BeBrogs <60266496+BeBrogs@users.noreply.github.com>
2020-03-09 08:05:49 +0000

    Update md5_sha1_sha2_sha3.py
    Fixed sha 3 bug

--- md5_sha1_sha2_sha3.py
+++ md5_sha1_sha2_sha3.py
@@ -2,18 +2,21 @@
 
 import pdb
 import hashlib
-#Importing hashing library contains SECURE hash & message algorithm for MD5/RFC-1321 SHA1 224 256 384 512
+#Importing hashing library contains SECURE hash & message algorithm >
 
 #Taking something to hash
 
 toHash = input ("Please enter what you want to hash: \n")
 
+ 
 #Cracked
 md5 = hashlib.md5()
 md5.update(toHash.encode()) #Passing encoded toHash to our dic 
-print ("\nYour input in MD5 hash: \nHexDigest: \n" + str(md5.hexdigest())) #Hex Digesting str fed
+print ("\nYour input in MD5 hash: \nHexDigest: \n" + str(md5.hexdige>
 print ("Digest: \n" + str(md5.digest()))
-
+ 
+#Digest is the binary hash, hexdigest is the string hash
+ 
 #Cracked
 mySha1 = hashlib.sha1()
 mySha1.update(toHash.encode())
@@ -21,7 +24,8 @@ print ("\n\nIn SHA1: \n"+ str(mySha1.hexdigest()))
 print ("Digest: \n" + str(mySha1.digest()))
 
 
-#SHA2 - Common hashes, secure for now - likely to be broken - matter of time
+print ("\n\nSHA2 Hashes: ")
+#SHA2 - Common hashes, secure for now - likely to be broken - matter>
 mySha224 = hashlib.sha224()
 mySha224.update(toHash.encode())
 print ("\n\nIn SHA224: \n" +str(mySha224.hexdigest()))
@@ -42,8 +46,19 @@ mySha512.update(toHash.encode())
 print ("\n\nIn SHA512: \n" + str(mySha512.hexdigest()))
 print ("Digest: \n" + str(mySha512.digest()))
 
-#SHA3 - introduced by NIST as backup plan for when SHA2 Cracks w/ sponge function different from sha 2 & 1
-#mySHA3_256 = hashlib.sha3_256()
-#mySHA3_256.update(toHash.encode())
-#print ("\n\nSHA3-256" + str(mySHA3_256.hexdigest()))
-#print ("Digest: \n" + str(mySHA3_256.digest()))
+                                                         print ("\n\nSHA3 Hashes: ")
+#SHA3 - introduced by NIST as backup plan for when SHA2 Cracks w/ sp>
+#SHA3 is unlikely to be broken anytime soon
+mySHA3_256 = hashlib.sha3_256()
+mySHA3_256.update(toHash.encode())
+print ("\n\nSHA3-256: \n" + str(mySHA3_256.hexdigest()))
+print("Digest: \n" + str(mySHA3_256.digest()))
+
+
+mySHA3_512 = hashlib.sha3_512()
+mySHA3_512.update(toHash.encode())
+print ("\n\nSHA3-512: \n" + str(mySHA3_512.hexdigest()))
+print("Digest: \n" + str(mySHA3_512.digest()))
+
+#ElFin
+
